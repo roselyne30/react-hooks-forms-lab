a5dccb1df904790729a478240a91acb6c3d00d7f
roselynelutomia <roselyne.lutomia@student.moringaschool.com>
2023-02-11 14:26:49 +0300

    MY Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,16 @@ function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+    setIsDarkMode((isDarkMode) => !isDarkMode); 
   }
-
+  function handleItemFormSubmit(newItem) {
+    setItems([...items, newItem]);
+  }
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
       <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handleItemFormSubmit} />
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -4,12 +4,12 @@ function Item({ name, category }) {
   const [isInCart, setIsInCart] = useState(false);
 
   function handleAddToCartClick() {
-    setIsInCart((isInCart) => !isInCart);
+    setIsInCart((isInCart) => !isInCart); 
   }
 
   return (
     <li className={isInCart ? "in-cart" : ""}>
-      <span>{name}</span>
+      <span>{name}</span> 
       <span className="category">{category}</span>
       <button
         className={isInCart ? "remove" : "add"}
